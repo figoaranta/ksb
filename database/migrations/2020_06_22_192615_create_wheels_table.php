b123ff8017a1fee31cddbab113eab8e2c39c8d78
Figo <figoaranta@gmail.com>
2020-09-23 22:51:39 +0700

    tidy up few things

--- database/migrations/2020_06_22_192615_create_wheels_table.php
+++ database/migrations/2020_06_22_192615_create_wheels_table.php
@@ -17,13 +17,13 @@ class CreateWheelsTable extends Migration
             $table->id();
             $table->string("uniqueCode")->unique();
             $table->string('brandType')->index;
-            $table->foreign('brandType')->references('brandType')->on('brandTypes');
+            // $table->foreign('brandType')->references('brandType')->on('brandTypes');
             $table->string('wheelSizeType');
-            $table->foreign('wheelSizeType')->references('wheelSizeType')->on('wheelSizeTypes');
+            // $table->foreign('wheelSizeType')->references('wheelSizeType')->on('wheelSizeTypes');
             $table->integer('ringSizeType');
-            $table->foreign('ringSizeType')->references('ringSizeType')->on('ringSizeTypes');
+            // $table->foreign('ringSizeType')->references('ringSizeType')->on('ringSizeTypes');
             $table->string('patternType');
-            $table->foreign('patternType')->references('patternType')->on('patternTypes');
+            // $table->foreign('patternType')->references('patternType')->on('patternTypes');
             $table->timestamps();
         });
     }
